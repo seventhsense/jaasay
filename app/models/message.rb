d3cff0cdd759bce22d7767fb8f6bbb1bfe3d8f44
seventh <yahihi@yahoo.co.jp>
2012-09-07 13:46:44 +0900

    add validation limit20

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,7 @@ class Message < ActiveRecord::Base
   attr_accessible :content, :template_id
   belongs_to :template
   validates :content,:presence => true
+  validates :content,:length => {
+    :maximum => 20
+  }
 end
